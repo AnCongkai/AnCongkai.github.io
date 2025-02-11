1b429d166df854ca2f5fb5de6fd8abd1926004e0
3DRX <3drxkjy@gmail.com>
2025-02-11 13:55:02 +0800

    tweak meta tag content

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -4,7 +4,9 @@ import config from "../../config.json";
 
 const configSchema = z.object({
   name: z.string(),
+  search_name: z.string(),
   sub_title: z.string(),
+  search_sub_title: z.string(),
   links: z.array(
     z.object({
       label: z.string(),
@@ -38,7 +40,9 @@ const configSchema = z.object({
 const typedSchema = configSchema.parse(config);
 
 export const NAME = typedSchema.name;
+export const SEARCH_NAME = typedSchema.search_name;
 export const SUB_TITLE = typedSchema.sub_title;
+export const SEARCH_SUB_TITLE = typedSchema.search_sub_title;
 export const LINKS: LinkItem[] = typedSchema.links;
 export const BIO_TEXT = typedSchema.bio_text;
 export const BIO_ITEMS: BioItem[] = typedSchema.bio_items;
